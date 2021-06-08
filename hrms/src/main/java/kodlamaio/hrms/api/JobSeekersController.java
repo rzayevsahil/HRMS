82a8d2772593d715732779bfbd518d360ad79f37
Sahil Rzayev <sahilrzayev2000@gmail.com>
2021-06-09 01:58:32 +0400

    JobSeeker tüm isterler tamamlandı

--- hrms/src/main/java/kodlamaio/hrms/api/JobSeekersController.java
+++ hrms/src/main/java/kodlamaio/hrms/api/JobSeekersController.java
@@ -1,30 +1,22 @@
 package kodlamaio.hrms.api;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.validation.FieldError;
 
 import kodlamaio.hrms.business.abstracts.JobSeekerService;
 import kodlamaio.hrms.core.utilities.DataResult;
-import kodlamaio.hrms.core.utilities.ErrorDataResult;
 import kodlamaio.hrms.entities.concretes.JobSeeker;
 
 @RestController
@@ -63,21 +55,6 @@ public class JobSeekersController {
 	@GetMapping("getById")
 	public DataResult<JobSeeker> getJobseekerByNationalId(@RequestParam int id){
 		return jobseekerService.getById(id);
-	}
-	
-	
-	
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	public ErrorDataResult<Object> handleValidationException(MethodArgumentNotValidException exceptions){
-		Map<String,String> validationErrors = new HashMap<String, String>();
-		
-		for(FieldError fieldError: exceptions.getBindingResult().getFieldErrors()) {
-			validationErrors.put(fieldError.getField(),fieldError.getDefaultMessage());			
-		}
-		
-		ErrorDataResult<Object> errors=new ErrorDataResult<Object>(validationErrors,"Doğrulama hataları");
-		return errors;
 	}
 	
 }
