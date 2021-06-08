ff648e0234808d7ee38fcd00b3d1635792a25ebe
Sahil Rzayev <sahilrzayev2000@gmail.com>
2021-06-09 02:01:39 +0400

    City tüm isterler yapıldı

--- hrms/src/main/java/kodlamaio/hrms/api/CitiesController.java
+++ hrms/src/main/java/kodlamaio/hrms/api/CitiesController.java
@@ -1,30 +1,22 @@
 package kodlamaio.hrms.api;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import kodlamaio.hrms.business.abstracts.CityService;
 import kodlamaio.hrms.core.utilities.DataResult;
-import kodlamaio.hrms.core.utilities.ErrorDataResult;
 import kodlamaio.hrms.entities.concretes.City;
 
 @RestController
@@ -57,20 +49,6 @@ public class CitiesController {
 	@DeleteMapping("delete")
 	public ResponseEntity<?> delete(@Valid @RequestParam int cityId) {
 		return ResponseEntity.ok(this.cityService.delete(cityId));
-	}
-	
-	
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	public ErrorDataResult<Object> handleValidationException(MethodArgumentNotValidException exceptions){
-		Map<String,String> validationErrors = new HashMap<String, String>();
-		
-		for(FieldError fieldError: exceptions.getBindingResult().getFieldErrors()) {
-			validationErrors.put(fieldError.getField(),fieldError.getDefaultMessage());			
-		}
-		
-		ErrorDataResult<Object> errors=new ErrorDataResult<Object>(validationErrors,"Doğrulama hataları");
-		return errors;
 	}
 	
 }
