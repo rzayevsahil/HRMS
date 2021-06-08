c6110abaaa160f1d8635dc945e69b0cde6893ff3
Sahil Rzayev <sahilrzayev2000@gmail.com>
2021-06-09 01:46:28 +0400

    Employer tüm istekler yapıldı

--- hrms/src/main/java/kodlamaio/hrms/api/EmployersController.java
+++ hrms/src/main/java/kodlamaio/hrms/api/EmployersController.java
@@ -1,30 +1,22 @@
 package kodlamaio.hrms.api;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import kodlamaio.hrms.business.abstracts.EmployerService;
 import kodlamaio.hrms.core.utilities.DataResult;
-import kodlamaio.hrms.core.utilities.ErrorDataResult;
 import kodlamaio.hrms.entities.concretes.Employer;
 
 @RestController
@@ -60,17 +52,4 @@ public class EmployersController {
 		return ResponseEntity.ok(this.employerService.delete(employerId));
 	}
 	
-	
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	public ErrorDataResult<Object> handleValidationException(MethodArgumentNotValidException exceptions){
-		Map<String,String> validationErrors = new HashMap<String, String>();
-		
-		for(FieldError fieldError: exceptions.getBindingResult().getFieldErrors()) {
-			validationErrors.put(fieldError.getField(),fieldError.getDefaultMessage());			
-		}
-		
-		ErrorDataResult<Object> errors=new ErrorDataResult<Object>(validationErrors,"Doğrulama hataları");
-		return errors;
-	}
 }
