0495a0541a95c016b132a4c5f924ce7f67a64d53
Sahil Rzayev <sahilrzayev2000@gmail.com>
2021-06-12 15:29:15 +0400

    JobSeekerDao updated ✅

--- hrms/src/main/java/kodlamaio/hrms/dataAccess/abstracts/JobSeekerDao.java
+++ hrms/src/main/java/kodlamaio/hrms/dataAccess/abstracts/JobSeekerDao.java
@@ -3,7 +3,6 @@ package kodlamaio.hrms.dataAccess.abstracts;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import kodlamaio.hrms.entities.concretes.JobSeeker;
 
