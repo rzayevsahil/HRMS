e6eea0fa45cfb0c0a15118cb30c2ca733e9c8612
Sahil Rzayev <sahilrzayev2000@gmail.com>
2021-06-18 23:03:08 +0400

    updated ✅

--- hrms/src/main/java/kodlamaio/hrms/api/JobAdvertsController.java
+++ hrms/src/main/java/kodlamaio/hrms/api/JobAdvertsController.java
@@ -9,7 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
