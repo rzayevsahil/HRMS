ae54e10fe5be2776d1170c22aeb70eaac432ca4e
Sahil Rzayev <sahilrzayev2000@gmail.com>
2021-06-20 02:26:46 +0400

    Gerçek mail e mesaj gönderme eklendi ✅

--- hrms/src/main/java/kodlamaio/hrms/business/concretes/AuthManager.java
+++ hrms/src/main/java/kodlamaio/hrms/business/concretes/AuthManager.java
@@ -80,7 +80,7 @@ public class AuthManager implements AuthService{
 			jobseekerService.add(jobseeker);
 			String code = verificationService.sendCode();
 			verificationCodeRecord(code, jobseeker.getId(), jobseeker.getEmail());
-			//mailSenderService.sendSimpleEmail(jobseeker.getEmail(), "Hrms verification code", code); //gerçek zamanlı mesaj gönderir
+			//mailSenderService.sendSimpleEmail(jobseeker.getEmail(), "Hrms verification code","Doğrulama kodunuz : " + code); //gerçek zamanlı mesaj gönderir
 			return new SuccessResult("Registration has been successfully completed");
 		}
 		
